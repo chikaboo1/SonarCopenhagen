2a59dad44a60220aa7fd5ac0fff6f1103223f3d1
chikaboo1 <chikaboo1@gmail.com>
2014-05-20 18:52:44 +0200

    adding stuff

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <meta name="viewport" content="width=device-width"/>
+		<!-- <script src="js/jquery-1.10.2.js"></script> -->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+         <script type="text/javascript" src="http://stratus.sc/stratus.js"></script>
+
+        <script type="text/javascript">
+  $(document).ready(function(){
+    $.stratus({
+      links: 'https://soundcloud.com/natasha-natarajan/sets/vega'
+    });
+  });
+</script>
+
+
+      </head>
+
+      <body>
+      	
+      	
+      </body>
+
+  </html>
