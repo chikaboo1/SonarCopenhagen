4713ecf02a2f130eaf0dec0466a7034b92b169e5
Luke Pommersheim <lpommers@gmail.com>
2014-05-24 19:20:34 +0200

    cleaning

---
+++ form.php
@@ -0,0 +1,11 @@
+
+  <form>
+    First Name: <input type='text' id ='firstname'> <br>
+    Last Name: <input type='text' id = 'lastname'> <br>
+    Email: <input type='text' id = 'email'> <br>
+    Artist Name<input type='text' id = 'artistname'> <br>
+    Track Title <input type="text" id ='tracktitle'> <br>
+    Sound Cloud User Name<input type="text" id = "soundname"> <br>
+    Site Link<input type="text" id = "sitelink"> <br>
+    <input type='button' value='submit' onclick="add();">
+  </form>
