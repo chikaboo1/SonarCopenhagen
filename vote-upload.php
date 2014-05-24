0b1219d040c94596e88982c3b335f550d6621f08
Luke Pommersheim <lpommers@gmail.com>
2014-05-24 17:43:01 +0200

    fixed and up to date php

--- vote-upload.php
+++ vote-upload.php
@@ -3,14 +3,36 @@
 
 include ("dbconnect.php");
 
-$voter = $_POST['posttest'];
+$contestant = $_POST['posttest'];
+$user = $_POST['postUser'];
 
 
-$voteSQL = "UPDATE contestants SET votes = votes + 1 WHERE ID ='$voter'";
+$allUsers = "SELECT facebook_ID FROM vote_upload";
+
+$userResult = mysql_query($allUsers);
+
+$userArray = mysql_fetch_array($userResult);
+
+if (in_array($user, $userArray)) {
+    echo "<script language=javascript> alert(\"this is where normally we'd tell you that you've already voted and that you can't vote again. But for the purposes of this prototype, we've allowed for multiple voting\");
+
+
+    </script>";
+  }
+
+
+
+$voterInformation = "INSERT INTO vote_upload (ID, facebook_ID, voted, contestant_ID) VALUES ('', '$user', '1', '$contestant')";
+
+mysql_query($voterInformation);
+
+
+
+$voteSQL = "UPDATE contestants SET votes = votes + 1 WHERE ID ='$contestant'";
 
 mysql_query($voteSQL);
 
-$grabNewVotes = "SELECT * FROM contestants WHERE ID ='$voter'";
+$grabNewVotes = "SELECT * FROM contestants WHERE ID ='$contestant'";
 $result = mysql_query($grabNewVotes);
 
  while($row = mysql_fetch_array($result)){
