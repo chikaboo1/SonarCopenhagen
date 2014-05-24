015f02a1727da0e313a09721cc8a2b55fa57897b
Luke Pommersheim <lpommers@gmail.com>
2014-05-24 18:01:34 +0200

    new db

--- mydb.sql
+++ mydb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 24, 2014 at 05:53 PM
+-- Generation Time: May 24, 2014 at 06:00 PM
 -- Server version: 5.5.33
 -- PHP Version: 5.5.3
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `mydb`
 --
+CREATE DATABASE IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `mydb`;
 
 -- --------------------------------------------------------
 
@@ -26,6 +28,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `contestants`
 --
 
+DROP TABLE IF EXISTS `contestants`;
 CREATE TABLE `contestants` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `firstname` varchar(45) NOT NULL,
@@ -47,9 +50,9 @@ CREATE TABLE `contestants` (
 --
 
 INSERT INTO `contestants` (`ID`, `firstname`, `lastname`, `email`, `artistname`, `tracktitle`, `soundcloud`, `sitelink`, `votes`) VALUES
-(1, 'Han', 'Solo', 'Han@starwars.com', 'DJ Solo', 'I Shot First', 'Han Solo on Soundcloud', 'blank site link for now', 50),
-(2, 'Luke', 'Skywalker', 'luke@skywalker.com', 'DJ Luke', 'Use the Force', 'DJ Luke on Soundcloud', 'www.lukepommersheim.com', 47),
-(3, 'Natasha', 'tashtasha', 'natasha@natasha.com', 'DJ natasha', 'Natasha bringing down the house', 'DJ Natasha on Soundcloud', 'some other link', 34),
+(1, 'Han', 'Solo', 'Han@starwars.com', 'DJ Solo', 'I Shot First', 'Han Solo on Soundcloud', 'blank site link for now', 51),
+(2, 'Luke', 'Skywalker', 'luke@skywalker.com', 'DJ Luke', 'Use the Force', 'DJ Luke on Soundcloud', 'www.lukepommersheim.com', 49),
+(3, 'Natasha', 'tashtasha', 'natasha@natasha.com', 'DJ natasha', 'Natasha bringing down the house', 'DJ Natasha on Soundcloud', 'some other link', 37),
 (4, 'Eythor', 'Iceland', 'Edog@eythor.com', 'DJ Eythy', 'Welcome to the land of Ice and Fire', 'Eythor on soundcloud', 'se;orgher', 27),
 (5, 'Renee', 'Renneeee', 'reee@reenee.com', 'DJ RR', 'Renee on the DJ', 'Renee', 'arlkghaelkrg', 32);
 
@@ -59,6 +62,7 @@ INSERT INTO `contestants` (`ID`, `firstname`, `lastname`, `email`, `artistname`,
 -- Table structure for table `vote_upload`
 --
 
+DROP TABLE IF EXISTS `vote_upload`;
 CREATE TABLE `vote_upload` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `facebook_ID` bigint(50) NOT NULL,
@@ -67,26 +71,14 @@ CREATE TABLE `vote_upload` (
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID_UNIQUE` (`ID`),
   KEY `fk_vote_upload_competition_enter_idx` (`contestant_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=119 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=120 ;
 
 --
 -- Dumping data for table `vote_upload`
 --
 
 INSERT INTO `vote_upload` (`ID`, `facebook_ID`, `voted`, `contestant_ID`) VALUES
-(106, 10152063862450474, 1, 4),
-(107, 10152063862450474, 1, 2),
-(108, 10152063862450474, 1, 2),
-(109, 10152063862450474, 1, 2),
-(110, 10152063862450474, 1, 3),
-(111, 10152063862450474, 1, 1),
-(112, 10152063862450474, 1, 1),
-(113, 10152058660971175, 1, 4),
-(114, 10152058660971175, 1, 2),
-(115, 10152058660971175, 1, 1),
-(116, 10152058660971175, 1, 1),
-(117, 10152058660971175, 1, 1),
-(118, 10152058660971175, 1, 2);
+(119, 10152063862450474, 1, 3);
 
 --
 -- Constraints for dumped tables
