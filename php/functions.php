6247af3eab8e7381ff7e7ade89da3c81d4972e67
chikaboo1 <chikaboo1@gmail.com>
2014-05-26 22:24:55 +0200

    home page competition same as mobile

--- php/functions.php
+++ php/functions.php
@@ -31,13 +31,13 @@ function top_four(){
 
 		//output it as hmtl
 		while($row = mysql_fetch_assoc($resultInfo)){
-			  echo "<div class = 'contestant table-box' id = '".  $row['ID'] . "'>";
-			  echo "<div class='contestant-image'><img src='".$row['image']."' alt='".$row['artistname']." image'/></div>";
-		 	 echo "<h3 class = 'contestantName'>" . $row['artistname']. "</h3>";
-			  echo "<p class = 'trackname'>" . $row['tracktitle'] . "</p>";
-			  echo "<div class='bottom-line'><p class='track-play'><a href='' class='stratus'><span class='play-empty'></span></p></a>";
-			  echo "<p class = 'votes'>" . $row['votes']."<button onclick = 'doLogin(" . $row['ID'] . ")'><span class='heart-empty'></span></button></p></div>";
-			  echo "</div>";
+			 echo "<div class = 'contestant table-box' id = '".  $row['ID'] . "'>";
+		   echo "<div class='contestant-image'><img src='".$row['image']."' alt='".$row['artistname']." image'/></div>";
+		  echo "<div class='box-text-wrapper'><h3 class = 'contestantName'>" . $row['artistname']. "</h3>";
+		  echo "<p class = 'trackname'>" . $row['tracktitle'] . "</p></div>";
+		  echo "<div class='bottom-line'><p class='track-play'><a href='' class='stratus'><span class='play-empty'></span></p></a>";
+		  echo "<p class = 'votes'>" . $row['votes']."<button onclick = 'doLogin(" . $row['ID'] . ")'><span class='heart-empty'></span></button></p></div>";
+		  echo "</div>";
 		}
 	}
 
