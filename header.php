d6e4000d46006ac693c8ab6374a9c1283bf66a46
chikaboo1 <chikaboo1@gmail.com>
2014-05-27 11:53:07 +0200

    media icons and header stuff

--- header.php
+++ header.php
@@ -95,7 +95,7 @@ include ("php/functions.php");
              <p class="nowrap desktop-show">4 days. 8 venues. 50 artists. 1 experience.</p>
              <p class="nowrap" id="sonarcph">Sonar Copenhagen</p>
              <p class="nowrap" id="sonardates">October 16th-19th</p>
-             <div class="buy"><button class="important-button buy-tickets desktop-show"><a href="tickets.php">Buy tickets</a></button></div><div id="socialmediaicons"><a href="https://www.facebook.com/SonarFestivalOfficialPage"><img class="nav-socialmedia" id="nav-facebook" src="images/social-media/facebook.png"/></a><a href="http://www.soundcloud.com/natasha-natarajan"><img class="nav-socialmedia"class="nav-socialmedia"  id="nav-soundcloud" src="images/social-media/soundcloud.png"/></a><a href="https://twitter.com/SonarFestival"><img class="nav-socialmedia" id="nav-twitter" src="images/social-media/twitter.png"/></a></div>
+             <p><div class="buy"><button class="important-button buy-tickets desktop-show"><a href="tickets.php">Buy tickets</a></button></div><div id="socialmediaicons"><a href="https://www.facebook.com/SonarFestivalOfficialPage"><img class="nav-socialmedia" id="nav-facebook" src="images/social-media/facebook.png"/></a><a href="http://www.soundcloud.com/natasha-natarajan"><img class="nav-socialmedia"class="nav-socialmedia"  id="nav-soundcloud" src="images/social-media/soundcloud.png"/></a><a href="https://twitter.com/SonarFestival"><img class="nav-socialmedia" id="nav-twitter" src="images/social-media/twitter.png"/></a></div></p>
             </div>
         </div>
           <nav class="desktop-show">
