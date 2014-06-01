db31ecc52844e29135867ab6314e9a17a5a75d12
Luke Pommersheim <lpommers@gmail.com>
2014-06-01 15:05:05 +0200

    maps / instagram / information page

--- information.php
+++ information.php
@@ -2,40 +2,6 @@
 include ('header.php');
 ?>
 
-<script type="text/javascript">
-    var feed = new Instafeed({
-        get: 'tagged',
-        tagName: 'pumpehuset',
-        clientId: '4604e28ba24e4582ab9dae943c927879'
-    });
-$(function(){
-	$('#instagram').click(function(){
-		var instafeed = document.getElementById("instafeed");
-		instafeed.innerHTML = feed.run();
-
-	});
-
-
-
-function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
-
-  var map = new google.maps.Map(document.getElementById('instafeed'),
-      mapOptions);
-}
-
-
-$('#address').click(function(){
-	var instafeed = document.getElementById("instafeed");
-		instafeed.innerHTML = initialize();
-});
-
-});
-</script>
-
 <div id="information">
 <h1>Information</h1>
 <p class="large">Sonar Copenhagen is a <span class="highlight-red">city festival</span> taking place in <span class="highlight-red">8 different venues</span> in Copenhagen's city center <span class="highlight-red">this Autumn</span> between October 16th and 19th 2014.</p>
@@ -55,8 +21,26 @@ $('#address').click(function(){
 <h3>Latest in <span>Huset</span></h3> -->
 
 <h1 id='instagram'>get the instagram</h1>
-<h id='address'1>get hte address</h1>
-<div id="instafeed">
+<h1 id='address'1>get hte address</h1>
+
+
+<h3>Pumpehuset</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra ipsum eu diam sodales fermentum. Ut eget feugiat neque.</p>
+<p>
+	<button type='submit' onclick="placeInstagram('pumpehuset');">instagram</button>
+	<button type= 'submit' onclick="placeMap(55.677294, 12.565205);">show map info</button>
+</p>
+
+<h3>Huset</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra ipsum eu diam sodales fermentum. Ut eget feugiat neque.</p>
+<p>
+	<button type='submit' onclick="placeInstagram('husetkbh');">instagram</button>
+	<button type= 'submit' onclick="placeMap(55.676549, 12.574690);">show map info</button>
+</p>
+
+
+
+<div id="instafeed" class="desktop-show">
 
 
 <h3>troighjrosigh;wrtsgn;f</h3>
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
  $(document).ready(function(){
 
-	
+
  	//playlist desktop
 
 	$("#playlist-wrapper-desktop").toggle(
@@ -12,7 +12,7 @@
 		function()
 
 		{
-			
+
 		    $('#playlist-wrapper-desktop').animate({left:'-350px'}, 200);
 
 		}
@@ -28,7 +28,7 @@
 	  $( "#playlist-wrapper-mobile" ).slideToggle('fast');
 	});
 
-	$('body').click(function (event) {    		
+	$('body').click(function (event) {
     $('#playlist-wrapper-desktop').animate({left:'-350px'}, 200);
     $('#playlist-wrapper-mobile').hide();
 	});
@@ -49,7 +49,7 @@
 
  	});
 
- 	//competition upload 
+ 	//competition upload
  	$("#contestant-upload").hide();
  	$("#contestant-upload-button").click(function(){
  		$("#contestant-upload").slideToggle();
@@ -57,3 +57,53 @@
  	});
 
   });
+
+
+//instagram
+var feed = new Instafeed({
+    get: 'tagged',
+    tagName: 'pumpehuset',
+    clientId: '4604e28ba24e4582ab9dae943c927879'
+});
+
+
+function createInstagram(venue){
+	var venue  = new Instafeed({
+		get : "tagged",
+		tagName : venue,
+		clientId: '4604e28ba24e4582ab9dae943c927879'
+	});
+
+	return venue.run();
+
+}
+function placeInstagram(venue){
+	var instafeed = document.getElementById("instafeed");
+		//instafeed.innerHTML = feed.run();
+		instafeed.innerHTML = createInstagram(venue);
+}
+
+//google mapes
+function initialize(lat, long) {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(lat, long)
+  };
+
+  var map = new google.maps.Map(document.getElementById('instafeed'),
+      mapOptions);
+
+  var myLatlng = new google.maps.LatLng(lat, long);
+
+
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title:"Hello World!"
+	});
+}
+
+function placeMap(lat, long){
+	var instafeed = document.getElementById("instafeed");
+		instafeed.innerHTML = initialize(lat, long);
+}
