526bc1e8203c71156c0c29aff1a7826b04eeac6d
chikaboo1 <chikaboo1@gmail.com>
2014-06-01 15:05:08 +0200

    added form inside page

--- competition.php
+++ competition.php
@@ -13,10 +13,23 @@ include ('header.php');
 
 <button id="contestant-upload-button" class="important-button">Enter the Competition Now</button>
 
-<?php
-include('php/form.php');
-?>
+  <form id="contestant-upload" action=''>
+    <h2>Fill in the following form to enter the competition today</h2>
+    <div class="input-wrapper"><p>First Name*</p> <input type='text' name ='firstname' id='test'>
+   <p>Last Name*</p> <input type='text' name = 'lastname'>
+    <p>Email*</p><input type='text' name = 'email'>
+    <p>Artist Name*</p><input type='text' name = 'artistname'>
+    <p>Track Title*</p><input type="text" name ='tracktitle'>
+    <p>SoundCloud link to track*</p><input type="text" name = "soundname">
+    <p>Link to website</p><input type="text" name = "sitelink">
+    <p>Artist image*</p><input type="file" name = 'image'> </div>
+    <div class="border"><p class=""> // Please only use .jpg, .gif, or .png files. </p>
+    <p> // Don't go over 2MB per image or a monster will get you!!!</p></div>
+    <div id="myDiv"></div>
+    <button type = 'submit' onclick = 'add();'>Upload</button>
+  </form>
 
+  
 <h2>Our Participants</h2>
 
 <p><a href="https://soundcloud.com/natasha-natarajan/sets/competition" class="stratus">Listen to the full playlist<span class="play-empty"></span></a></p>
