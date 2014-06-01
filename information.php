17565bae8b10bec5d16d631209d52df5674b921c
Luke Pommersheim <lpommers@gmail.com>
2014-06-01 16:14:33 +0200

    dummy text

--- information.php
+++ information.php
@@ -12,41 +12,41 @@ include ('header.php');
 <div id="google-maps"><iframe src="https://mapsengine.google.com/map/embed?mid=z5BHJ4rKxSGI.k2PngD4VyH68"></iframe></div>
 
 <h3>Huset</h3>
-<p>blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text</p>
-<p><a href="http://www.huset-kbh.dk">the website</a> / address text address text address text</p>
+<p>Neutra shabby chic butcher locavore Williamsburg selfies. Flexitarian Godard scenester DIY farm-to-table biodiesel lomo. Yr American Apparel Carles farm-to-table mumblecore.</p>
+<p><a href="http://www.huset-kbh.dk">the website</a> / Rådhusstræde 13, København K, Danmark</p>
 
 <h3>Pumpehuset</h3>
-<p>blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text</p>
-<p><a href="http://www.pumpehuset.dk">the website</a> / address text address text address text</p>
+<p>Jean shorts flannel post-ironic Helvetica. 3 wolf moon pour-over synth normcore Austin.</p>
+<p><a href="http://www.pumpehuset.dk">the website</a> / Studiestræde 52, København V, Danmark</p>
 
 
 <h3>Loppen</h3>
-<p>blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text</p>
-<p><a href="http://www.loppen.dk">the website</a> / address text address text address text</p>
+<p>Skateboard actually banh mi, vegan blog seitan art party bicycle rights quinoa Echo Park cornhole plaid selfies drinking vinegar Etsy.</p>
+<p><a href="http://www.loppen.dk">the website</a> / Sydområdet 4B, København, Danmark</p>
 
 
 <h3>Lille Vega</h3>
-<p>blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text</p>
-<p><a href="http://www.vega.dk">the website</a> / address text address text address text</p>
+<p>Pug Tonx dreamcatcher four loko, lo-fi beard bicycle rights Cosby sweater. Blue Bottle tousled occupy, lomo beard cred High Life 90's banh mi direct trade. </p>
+<p><a href="http://www.vega.dk">the website</a> / Enghavevej 40, København V, Danmark</p>
 
 <h3>Store Vega</h3>
-<p>blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text</p>
-<p><a href="http://www.vega.dk">the website</a> / address text address text address text</p>
+<p>Crucifix paleo Thundercats, quinoa meh swag Etsy tote bag next level trust fund. Single-origin coffee Carles swag, lomo hoodie forage seitan.</p>
+<p><a href="http://www.vega.dk">the website</a> / Enghavevej 40, København V, Danmark</p>
 
 
 <h3>Forum</h3>
-<p>blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text</p>
-<p><a href="http://www.forumcopenhagen.dk">the website</a> address text address text address text</p>
+<p>Truffaut Schlitz Tumblr lo-fi, cray authentic meh Godard banjo. Echo Park banh mi yr kitsch roof party pickled Tonx. Tote bag farm-to-table VHS post-ironic selfies brunch.</p>
+<p><a href="http://www.forumcopenhagen.dk">the website</a>Julius Thomsens Plads 1, Frederiksberg C, Danmark</p>
 
 
 <h3>Culturebox</h3>
-<p>blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text</p>
-<p><a href="http://www.culture-box.com">the website</a> / address text address text address text</p>
+<p>Polaroid Tumblr PBR&B skateboard. Scenester gastropub ugh jean shorts readymade normcore.</p>
+<p><a href="http://www.culture-box.com">the website</a> / Kronprinsessegade 54, Copenhagen K, Danmark</p>
 
 
 <h3>Rust</h3>
 <p>blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text blurb text</p>
-<p><a href="http://www.rust.dk">the website</a> / address text address text address text</p>
+<p><a href="http://www.rust.dk">the website</a> / Guldbergsgade 8, København N, Danmark</p>
 
 
 <div id="instafeed" class="desktop-show">
@@ -56,12 +56,36 @@ include ('header.php');
 
 
 <h2 class="border-top" id="accommodation">Accommodation</h2>
-
-<p></p>
+<h3>Hostels:</h3>
+<div class='hostel'>
+	<h4>Generator:</h4>
+	<p>Praesent imperdiet arcu ante, nec mattis elit semper vitae. Donec eu metus vel magna feugiat vulputate.
+	</p>
+</div>
+<br>
+<div class='hostel'>
+	<h4>Some Hostel:</h4>
+	<p>Praesent imperdiet arcu ante, nec mattis elit semper vitae. Donec eu metus vel magna feugiat vulputate.
+	</p>
+</div>
+<br>
+<div class='hostel'>
+	<h4>The Last Hostel:</h4>
+	<p>Praesent imperdiet arcu ante, nec mattis elit semper vitae. Donec eu metus vel magna feugiat vulputate.
+	</p>
+</div>
+<br>
 
 <h2 class="border-top" id="travel">Travel</h2>
+<h3>By Plane:</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nisi est, convallis sit amet commodo vitae, egestas nec felis. Sed suscipit odio arcu,</p>
 
-<p></p>
+<h3>By Car:</h3>
+<p>eget dictum eros faucibus a. Nullam eu feugiat neque, nec accumsan massa. Quisque mollis egestas massa, sed venenatis tortor lacinia sit amet. Cras egestas quam quis augue condimentum</p>
+
+<h3>By Hot Air Balloon:</h3>
+<p>a elementum justo suscipit. Mauris vestibulum molestie urna at ullamcorper. Pellentesque in est vel erat facilisis tristique vitae et velit.</p>
+
 
 
 </div>
